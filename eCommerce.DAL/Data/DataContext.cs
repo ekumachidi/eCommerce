c223ea84d2fe50e56f4168db9b1b2f1670696acc
Ekuma Chidi <bukki.arise@yahoo.co.uk>
2017-03-16 13:19:50 +0100

    Adds DbContext to DAL

---
+++ eCommerce.DAL/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using eCommerce.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.DAL.Data
+{
+    class DataContext : System.Data.Entity.DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
