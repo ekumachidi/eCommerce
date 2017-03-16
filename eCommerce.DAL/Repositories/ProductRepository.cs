509ebad8545e3786343da97ac80ccfd36c2ac45f
Ekuma Chidi <bukki.arise@yahoo.co.uk>
2017-03-16 18:20:22 +0100

    Adds repository files

--- eCommerce.DAL/Repositories/ProductRepository.cs
+++ eCommerce.DAL/Repositories/ProductRepository.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using eCommerce.DAL.Data;
+using eCommerce.Model;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,45 @@ namespace eCommerce.DAL.Repositories
 {
     class ProductRepository
     {
+        internal DataContext context;
+
+        public ProductRepository(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public virtual Product GetById(object id)
+        {
+            return context.Products.Find(id);
+        }
+
+        public virtual IQueryable<Product> GetAll()
+        {
+            return context.Products;
+        }
+        public virtual void Insert(Product entity)
+        {
+            context.Products.Add(entity);
+        }
+
+        public virtual void Update(Product entity)
+        {
+            context.Products.Attach(entity);
+            context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public virtual void Delete(Product entity)
+        {
+            if (context.Entry(entity).State == EntityState.Detached)
+            {
+                context.Products.Attach(entity);
+            }
+            context.Products.Remove(entity);
+        }
+
+        public virtual void Commit()
+        {
+
+        }
     }
 }
--- eCommerce.DAL/Repositories/RepositoryBase.cs
+++ eCommerce.DAL/Repositories/RepositoryBase.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using eCommerce.DAL.Data;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace eCommerce.DAL.Repositories
 {
-    class RepositoryBase
+    abstract class RepositoryBase<TEntity> where TEntity
     {
+        internal DataContext context;
+        internal DbSet<TEntity> dbSet;
     }
 }
