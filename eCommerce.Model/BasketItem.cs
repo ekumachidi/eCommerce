636adf68d21bfb8dd06bf189475763d2f610b27d
Ekuma Chidi <bukki.arise@yahoo.co.uk>
2017-03-29 13:09:44 +0100

    Adds Basket and BasketItem models

---
+++ eCommerce.Model/BasketItem.cs
@@ -0,0 +1,11 @@
+﻿namespace eCommerce.Model
+{
+    public class BasketItem
+    {
+        public int BasketItemId { get; set; }
+        public int BasketId { get; set; }
+        public int ProductId { get; set; }
+
+        public Product Product { get; set; }
+    }
+}
