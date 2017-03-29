25e8f82e76b22a3a3d773b19397c106fab63bf66
Ekuma Chidi <bukki.arise@yahoo.co.uk>
2017-03-29 05:55:34 +0100

    Adds RepositoryBase interface

---
+++ eCommerce.DAL/Repositories/IRepositoryBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace eCommerce.DAL.Repositories
+{
+    public interface IRepositoryBase<TEntity> where TEntity : class
+    {
+        void Commit();
+        void Delete(object id);
+        void Delete(TEntity entity);
+        void Dispose();
+        IQueryable<TEntity> GetAll();
+        IQueryable<TEntity> GetAll(object filter);
+        TEntity GetById(object id);
+        TEntity GetFullObject(object id);
+        IQueryable<TEntity> GetPaged(int top = 20, int skip = 0, object orderBy = null, object filter = null);
+        void Insert(TEntity entity);
+        void Update(TEntity entity);
+    }
+}
